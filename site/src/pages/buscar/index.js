b79e34f018c256b2ffe18ebab9dd50bfec17c6f7
Manfred-Charizard <pedtogabriela@gmail.com>
2022-06-24 01:56:19 -0300

    comite

---
+++ site/src/pages/buscar/index.js
@@ -0,0 +1,13 @@
+import Cabe from '../../components/cabeçalho'
+
+export default function Index(){
+    return(
+        <main>
+            <Cabe />
+            <div>
+                <h1>Ótima escolha companheiro, veio no lugar exato</h1>
+            
+            </div>
+        </main>
+    )
+}
---
+++ site/src/pages/inserir/index.js
@@ -0,0 +1,13 @@
+import Cabe from '../../components/cabeçalho'
+
+export default function Index(){
+    return(
+        <main>
+            <Cabe />
+            <div>
+                <h1>Vamos cadastrar o anime </h1>
+            
+            </div>
+        </main>
+    )
+}
